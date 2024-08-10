b24436ed557c218ba50adcc780dc61ad6b977d70
tasjen <129994991+tasjen@users.noreply.github.com>
2024-08-11 04:45:18 +0700

    Increase the z-index of Header

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@ const store = useStore();
 
 <template>
   <div
-    class="sticky top-0 bg-background pt-4"
+    class="sticky top-0 bg-background pt-4 z-50"
     :class="!store.isAtTop && 'shadow-md'"
   >
     <div class="mx-auto max-w-[2400px] px-6 md:px-8 xl:px-20">
